811216890e11a840455332065b219b44753dd947
anniebuckles <107632534+anniebuckles@users.noreply.github.com>
2022-06-17 10:44:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,4 +30,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice.normalized)
+streamlit.dataframe(fruityvice_normalized)
